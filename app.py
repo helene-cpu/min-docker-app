0b91d8ec7d62e6a8c6652968c2ec73f07be67554
helene-cpu <66snahel@stud.akademiet.no>
2026-03-26 15:16:28 +0100

    chat side

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/read")
+def read():
+    return render_template('read.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
