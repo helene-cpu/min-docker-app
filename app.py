7cdfd39b6df9d9db8fa3b9291a311a62e2c0cbe6
helene-cpu <66snahel@stud.akademiet.no>
2026-04-23 14:26:26 +0200

    Opprettet database med database forbindelse

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, session, redirect
-import mysql.connector
 from forms import ChatForm
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "secretkey"
+
+def init_db():
+    conn = sqlite3.connect('my_database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS Passord (ID INTEGER AUTOINCREMENT PRIMARY KEY, Passord TEXT)""")
+
+    cursor.execute("SELECT COUNT(*) FROM Passord")
+    if cursor.fetchone()[0] == 0:
+        cursor.execute("INSERT INTO Passord (Passord) VALUES (?, ?)", ("max", "Max"))
+    conn.commit()
+    conn.close()
 
 @app.route("/")
 def home():
@@ -14,6 +26,15 @@ def read():
     form = ChatForm()
     if form.validate_on_submit():
         chat = form.chat.data
+
+        conn = sqlite3.connect('my_database.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM Passord WHERE Passord = ?",  (chat,))
+        user = cursor.fetchall()
+
+        conn.commit()
+        conn.close()
 
         if user:
             return redirect("/virus")
