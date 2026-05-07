74c7b0b7d3ab879f75b47b3a469243be3ebbe4bc
helene-cpu <66snahel@stud.akademiet.no>
2026-05-07 09:35:56 +0200

    Implementert ett API som teller ned tiden

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, jsonify
 from forms import ChatForm
 import sqlite3
+import requests
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 app.secret_key = "secretkey"
@@ -13,9 +16,23 @@ def init_db():
 
     cursor.execute("SELECT COUNT(*) FROM Passord")
     if cursor.fetchone()[0] == 0:
-        cursor.execute("INSERT INTO Passord (Passord) VALUES (?, ?)", ("max", "Max"))
+        cursor.execute("""INSERT INTO Passord (Passord) VALUES (?)""", ("max",))
     conn.commit()
     conn.close()
+
+def get_current_time(timezone="Europe/Oslo"):
+    url = f"https://timeapi.io/api/Time/current/zone?timeZone={timezone}"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+
+        return data["dateTime"]
+
+    except Exception as e:
+        print("Error fetching time:", e)
+        return None
 
 @app.route("/")
 def home():
@@ -31,7 +48,7 @@ def read():
         cursor = conn.cursor()
 
         cursor.execute("SELECT * FROM Passord WHERE Passord = ?",  (chat,))
-        user = cursor.fetchall()
+        user = cursor.fetchone()
 
         conn.commit()
         conn.close()
@@ -45,7 +62,13 @@ def read():
 
 @app.route("/virus", methods=["POST", "GET"])
 def virus():
-    return render_template("virus.html")
+    current_time = get_current_time()
+
+    current_time = datetime.fromisoformat(current_time)
+
+    end_time = current_time + timedelta(minutes=10)
+
+    return render_template("virus.html", end_time=end_time.isoformat())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
